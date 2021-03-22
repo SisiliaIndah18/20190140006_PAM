eb03b221914024ad8167e4109b82c2e6f5018fda
SisiliaIndah18 <79432738+SisiliaIndah18@users.noreply.github.com>
2021-03-22 10:52:28 +0700

    Initial commit

---
+++ app/src/main/java/com/example/praktikumpam/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.praktikumpam;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnLogin;
+    EditText edemail, edpassword;
+    String EmailKey, PwKey;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.LoginBtn);
+        edemail = findViewById(R.id.IdEmail);
+        edpassword = findViewById(R.id.IdPw);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                EmailKey = "ndh1994@gmail.com";
+                PwKey = "2001";
+
+                if (EmailKey.equals(edemail.getText().toString()) && PwKey.equals(edpassword.getText().toString())){
+                    Toast s = Toast.makeText(getApplicationContext(),
+                            "Login Sukses Email Anda : " +EmailKey+ " dan Password Anda : "+PwKey+ "", Toast.LENGTH_LONG);
+                    s.show();
+
+                    Bundle b = new Bundle();
+                    b.putString("a", EmailKey.trim());
+                    b.putString("b", PwKey.trim());
+
+                    Intent intentBaru = new Intent( MainActivity.this, page2.class);
+                    intentBaru.putExtras(b);
+                    startActivity(intentBaru);
+                }
+
+                else if (!EmailKey.equals(edemail.getText().toString()) && !PwKey.equals(edpassword.getText().toString())){
+                    Toast s = Toast.makeText(getApplicationContext(),
+                            "Email dan Password Salah !!", Toast.LENGTH_SHORT);
+                    s.show();
+                }
+            }
+
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/praktikumpam/page2.java
@@ -0,0 +1,26 @@
+package com.example.praktikumpam;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class page2 extends AppCompatActivity {
+    TextView txEmail, txPassword;
+    String nama;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.page2);
+
+        txEmail = (TextView) findViewById(R.id.tvuser);
+        txPassword = findViewById(R.id.tvpw);
+        Bundle b2 = this.getIntent().getExtras();
+        String email = b2.getString("a");
+        String pass = b2.getString("b");
+
+        txEmail.setText(email);
+        txPassword.setText(pass);
+
+    }
+}
